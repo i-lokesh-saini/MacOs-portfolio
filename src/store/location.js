a56847f0b0dc0001c1d079f8f363a7db094568a3
Lokesh Saini <ls0647515@gmail.com>
2026-07-15 03:11:28 +0530

    done with resume and finder tab

---
+++ src/store/location.js
@@ -0,0 +1,17 @@
+import { locations } from "#constants";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+const DEFAULT_LOCATION = locations.work;
+
+const useLocationStore = create(immer((set) => ({
+    activeLocation: DEFAULT_LOCATION,
+    setActiveLocation: (location = null) => set((state) => {
+        state.activeLocation = location;
+    }),
+    resetActiverLocation: () => set((state) => {
+        state.activeLocation = DEFAULT_LOCATION;
+    }),
+})))
+
+export default useLocationStore;
